4313f86d626cd805ed3f851adbf87c5b7d7d4939
Thomas Merl <thomasmerl@gmx.de>
2025-06-16 16:55:47 +0200

    added method to retrieve available stocks

--- apps/backend/src/stocks/stockprice.controller.ts
+++ apps/backend/src/stocks/stockprice.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { IStockPriceProviderService } from './i-stock-price-provider-service';
+import { StockAvailabilityDTO } from '@stocksexample/shared/dist/DTO/StockAvailabilityDTO';
 
 @Controller('stocks')
 export class StockPriceController {
   constructor(private readonly appService: IStockPriceProviderService) {}
 
   @Get('availableStocks')
-  getHello(): string {
-
+  getAvailableStocks(): StockAvailabilityDTO {
+    return {
+      stocks: this.appService.getAvailableStocks(),
+    };
   }
 }
