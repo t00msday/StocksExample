d473e88ee5a238393bab8689522c97860c4004e2
Thomas Merl <thomasmerl@gmx.de>
2025-06-19 09:45:30 +0200

    injecting mockservice if called with matching arguments

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,12 +3,19 @@ import { FinnhubStockService } from './stocks/finnhub/finnhub-stock.service';
 import { HttpModule } from '@nestjs/axios';
 import { StockPriceController } from './stocks/stockprice.controller';
 import { IStockPriceProviderService } from './stocks/i-stock-price-provider-service';
+import * as process from 'node:process';
+import { MockStockService } from './stocks/mock/mock-stock.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [StockPriceController],
   providers: [
-    { provide: IStockPriceProviderService, useClass: FinnhubStockService },
+    {
+      provide: IStockPriceProviderService,
+      useClass: process.argv.includes('[stockprovider=mock]')
+        ? MockStockService
+        : FinnhubStockService,
+    },
   ],
 })
 export class AppModule {}
